55500e6073b07b878c14cdd9d97336d2966d0980
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-06-09 22:44:03 +0600

    social icon component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
+import SocialIcon from "./components/SocialIcon";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Home></Home>
+
+      <SocialIcon></SocialIcon>
       
     </div>
   );
